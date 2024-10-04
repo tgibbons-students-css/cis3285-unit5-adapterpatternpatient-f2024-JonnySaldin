ba4a8e3959acc2a36a8f96d81b0500c8d5864484
Jonny Saldin <jsaldin@css.edu>
2024-10-04 15:55:16 -0500

    Assignment 5 push

---
+++ InsuranceSystem/OutNetworkAdapter.cs
@@ -0,0 +1,35 @@
+﻿namespace Unit5_AdapterPatternPatient_Blazor.InsuranceSystem
+{
+    public class OutNetworkAdapter : InsuranceInterface
+    {
+        OutNetworkPatient patient;
+        public OutNetworkAdapter(string newName, int newPolicyNumber) { 
+            patient = new OutNetworkPatient(newName, newPolicyNumber);
+        }
+        public decimal CoverageAmount(int ProcedureID, decimal ProcedureCost)
+        {
+            decimal coveragePercent = patient.CoveragePercent(ProcedureCost);
+            return (coveragePercent * ProcedureCost);
+        }
+
+        public string getPatientName()
+        {
+            return patient.getPatientName();
+        }
+
+        public string getPolicyNumber()
+        {
+            return patient.PolicyNumber.ToString();
+        }
+
+        public bool IsCovered(string patientName, string policyNumber)
+        {
+            if (int.TryParse(policyNumber, out int parsedPolicyNumber))
+            {
+                return patient.IsCovered(patientName, parsedPolicyNumber) == "yes";
+            }
+
+            return false;
+        }
+    }
+}
